807e3390b7a96e413877351e6ccb4d75dca722d5
jamesxdaniel <jamesdaniel288@gmail.com>
2024-07-19 06:37:20 +0800

    chore: Update Nuxt configuration and workflow for GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,28 @@
 export default defineNuxtConfig({
 	ssr: true,
-  	css: ['~/assets/style.css'],
-    app: {
-      head: {
+	css: ['~/assets/style.css'],
+
+	app: {
+		head: {
 			title: 'James Daniel Enovero - Web Developer',
-            htmlAttrs: {
-                lang: 'en'
-            },
-            link: [
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/favicon.ico'
-                }
-            ],
-            script: [
-                {
-                    src: 'https://smtpjs.com/v3/smtp.js',
-                    defer: true
-                }
-            ]
-        }
-    }
+			htmlAttrs: {
+				lang: 'en'
+			},
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/x-icon',
+					href: '/favicon.ico'
+				}
+			],
+			script: [
+				{
+					src: 'https://smtpjs.com/v3/smtp.js',
+					defer: true
+				}
+			]
+		}
+	},
+
+	compatibilityDate: '2024-07-19'
 })
